448537c418bb4b06e523051d8d3c7393c0320b8e
mmarti22 <marina.marti.sierra@gmail.com>
2025-10-21 11:12:58 +0200

    feat(Js): added login sentences

---
+++ src/JavaScript/js.js
@@ -0,0 +1,18 @@
+//Variables
+const url= "https://api.adviceslip.com/advice";
+
+const adviceText = document.getElementById('adviceText');
+const adviceBtn = document.getElementById('ball-img');
+
+async function getAdvice() {
+    const res = await fetch(`${url}?timestamp=${Date.now()}`);
+    const data = await res.json();
+    const advice = data.slip.advice;
+    console.log(advice)
+    adviceText.innerText = advice;
+}
+
+getAdvice()
+
+adviceBtn.addEventListener('click', getAdvice);
+
